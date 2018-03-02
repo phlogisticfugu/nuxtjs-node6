e4d14aa96a0344e9cde66ff8e531563f4b4a2e41
Brad Ito <phlogisticfugu@gmail.com>
2018-03-02 01:32:42 -0800

    initial babelization of nuxt 1.0.1

---
+++ build.js
@@ -0,0 +1,15 @@
+const shell = require('shelljs')
+
+shell.rm('-rf', 'lib')
+shell.mkdir('lib')
+
+shell.cp('-r', 'node_modules/nuxt/lib/app', 'lib/app')
+shell.exec('npx babel node_modules/nuxt/lib/builder --out-dir ./lib/builder')
+shell.exec('npx babel node_modules/nuxt/lib/common --out-dir ./lib/common')
+shell.exec('npx babel node_modules/nuxt/lib/core --out-dir ./lib/core')
+shell.exec('npx babel node_modules/nuxt/lib/index.js --out-file ./lib/index.js')
+
+shell.rm('-rf', 'bin')
+// TODO use babel to be safe, need to find way to remove shebang nicely
+shell.cp('-r', 'node_modules/nuxt/bin', 'bin')
+
