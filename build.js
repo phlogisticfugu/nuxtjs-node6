40eb90ca40f25950e506c34abbc4d9696f6fdc96
Brad Ito <phlogisticfugu@gmail.com>
2018-03-05 01:03:43 -0800

    v1.0.2 adding util.promisify shim

--- build.js
+++ build.js
@@ -2,12 +2,13 @@ const shell = require('shelljs')
 
 shell.rm('-rf', 'lib')
 shell.mkdir('lib')
+shell.cp('src/index.js', 'lib/index.js')
 
 shell.cp('-r', 'node_modules/nuxt/lib/app', 'lib/app')
 shell.exec('npx babel node_modules/nuxt/lib/builder --out-dir ./lib/builder')
 shell.exec('npx babel node_modules/nuxt/lib/common --out-dir ./lib/common')
 shell.exec('npx babel node_modules/nuxt/lib/core --out-dir ./lib/core')
-shell.exec('npx babel node_modules/nuxt/lib/index.js --out-file ./lib/index.js')
+shell.exec('npx babel node_modules/nuxt/lib/index.js --out-file ./lib/index-nuxt.js')
 
 shell.rm('-rf', 'bin')
 // TODO use babel to be safe, need to find way to remove shebang nicely
